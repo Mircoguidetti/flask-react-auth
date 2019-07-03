cc432ee34264584f901a7d2e3dfda7282facd018
Mirco Guidetti <mirco@guidetti.co>
2019-07-03 15:50:48 +0100

    Connect react with redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import IndexRouter from './routers/IndexRouter';
+import configStore from './store/configStore';
+
+const store = configStore();
 
 const jsx = (
-    <div>
+    <Provider store={store}>
         <IndexRouter />
-    </div>
+    </Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app')); 
