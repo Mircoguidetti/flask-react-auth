8635a71bdc832b183a5b31f7927b3c6da29078a8
Mirco Guidetti <mirco@guidetti.co>
2019-07-12 11:29:25 +0100

    Config deployment

--- manage.py
+++ manage.py
@@ -1,12 +1,11 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import Command, Option
 
 from application import app, db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
 
 @manager.command
 def create_db():
@@ -16,3 +15,4 @@ def create_db():
 
 if __name__ == '__main__':
     manager.run()
+
